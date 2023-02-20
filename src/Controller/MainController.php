4744207e3d2973aa766b8682c5a162a49b60d44d
Guiyom2vo <guillaume2vo@yandex.ru>
2023-02-20 18:20:46 +0100

    Misa a jour CSS et JS

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,7 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+   
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);

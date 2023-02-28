684b2ecf42438a8af1f7d3ac0866cfeb5c4b7d4c
Guiyom2vo <guillaume2vo@yandex.ru>
2023-02-28 14:55:02 +0100

    AJAX

--- src/Controller/Http/ModalController.php
+++ src/Controller/Http/ModalController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Http;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ModalController extends AbstractController
@@ -13,10 +14,18 @@ class ModalController extends AbstractController
     #[Route('/ajax/addtocart', name: 'app_modal_addtocart')]
     public function index(Request $request, ProductRepository $productRepository)
     {
-        $id = $request->query->get('id');
+        if($request->isXmlHttpRequest() == true) {
+            $ModalAddToCart = $productRepository->find($request->query->get('id'));
 
-         //$ModalAddToCart = $this->getDoctrine()->getRepository(Product::class)->find($id);
-         $ModalAddToCart = $productRepository->find($id);
-         dd($ModalAddToCart);
+            // A prevoir l'envoie en sessions pour notre pannier
+
+            $DonneesAEnvoyerALaModal = [
+                'image'  => $ModalAddToCart->getPictures()->getValues()[0]->getimageName(),
+                'Titre'  => $ModalAddToCart->getTitle(),
+                'Slug'   => $ModalAddToCart->getSlug(),
+            ];
+
+            return new JsonResponse($DonneesAEnvoyerALaModal);
+        }     
     }
 }

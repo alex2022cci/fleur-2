d59e4d2e91b4c1924003ea7a0f70cefcee84de7b
Guiyom2vo <guillaume2vo@yandex.ru>
2023-03-01 12:47:50 +0100

    pannier

--- src/Services/AddtocartServices.php
+++ src/Services/AddtocartServices.php
@@ -2,17 +2,21 @@
 
 namespace App\Services;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class AddtocartServices
 {
     private RequestStack $requestStack;
+    private EntityManagerInterface $em;
     
-    public function __construct(RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack, EntityManagerInterface $em)
     {
         $this->requestStack = $requestStack;
+        $this->em = $em;
     }
 
     public function add(int $id){
@@ -24,6 +28,43 @@ class AddtocartServices
         }
         $this->getSession()->set('cart', $cart);
 
+    }
+
+    public function getFullCart(): array
+    {
+        $cart = $this->getSession()->get('cart');
+        $cartData = [];
+
+        foreach ($cart as $id => $quantity) {
+            $product = $this->em->getRepository(Product::class)->findOneBy(['id' => $id]);
+            if (!$product) {
+                // condition en cas d'erreur
+                // par exemple, le produit n'existe pas
+                // produit n est plus en stock dans les secondes qui suivent l'ajout au panier
+                // etc.
+            }
+
+            $cartData[] = [
+                'produit' => $product,
+                'quantity' => $quantity,
+            ];
+        }
+
+        return $cartData;
+    }
+
+    public function getTotal(): float
+    {
+        $total = 0;
+
+        foreach($this->getFullCart() as $item) {
+
+            // prevoir les promotions
+
+            $total += $item['produit']->getPrice() * $item['quantity'];
+        }
+
+        return $total;
     }
 
     private function getSession(): SessionInterface

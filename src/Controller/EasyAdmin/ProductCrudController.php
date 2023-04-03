e66540d02e63a19cede390eebde2f1e5a2d66e5c
Guiyom2vo <guillaume2vo@yandex.ru>
2023-04-03 11:10:16 +0200

    MAJ

--- src/Controller/EasyAdmin/ProductCrudController.php
+++ src/Controller/EasyAdmin/ProductCrudController.php
@@ -4,11 +4,16 @@ namespace App\Controller\EasyAdmin;
 
 use App\Entity\Product;
 use App\Form\ImageFieldType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\DateTimeFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
@@ -19,7 +24,22 @@ class ProductCrudController extends AbstractCrudController
     {
         return Product::class;
     }
+    
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Liste des Produits')
+            ->setEntityLabelInSingular('Product')
+            ->setDefaultSort(['id' => 'ASC']);
+    }
 
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(DateTimeFilter::new('CreatedAt', 'Date de création'))
+            ->add(TextFilter::new('Title', 'Nom du produit'))
+            ->add(EntityFilter::new('Category', 'Nom de la catégorie'));
+    }
 
     public function configureFields(string $pageName): iterable
     {

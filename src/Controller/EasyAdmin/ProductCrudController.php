b396213d8af50d2f39b1e67926e1522cf1c90961
Guiyom2vo <guillaume2vo@yandex.ru>
2023-04-03 10:33:02 +0200

    MAJ

--- src/Controller/EasyAdmin/ProductCrudController.php
+++ src/Controller/EasyAdmin/ProductCrudController.php
@@ -6,7 +6,6 @@ use App\Entity\Product;
 use App\Form\ImageFieldType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
